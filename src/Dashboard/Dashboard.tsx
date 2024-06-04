d7076c8d523f8ca4e1a104365f82ae0a6f7c47c0
Kim Novak <kimnovak187@gmail.com>
2024-06-04 07:27:41 +0200

    [feat] Remove highlighted courses

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,10 +1,8 @@
 import { Courses } from '../Courses/Courses';
-import { HighlightedCourses } from '../Courses/HighlightedCourses/HighlightedCourses';
 
 export const Dashboard = () => {
   return (
     <>
-      <HighlightedCourses />
       <Courses />
     </>
   );
